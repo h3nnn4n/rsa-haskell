07863f75d359454a07c92a4c37830226b02a924e
h3nnn4n <uber.renan@gmail.com>
2015-05-30 20:48:07 +0000

    Cleanup

--- genKeys.hs
+++ genKeys.hs
@@ -7,7 +7,6 @@ import System.IO.Error
 wrapper k = return $ genKeys k
 
 main = do
-    -- Gets the arguments, first one is the name for keyfile and the second is the key size
     args <- getArgs
     keys <- wrapper (read (args !! 1) :: Integer)
     let name = args !! 0
@@ -17,7 +16,4 @@ main = do
     print privName
     writeFile pubName  $ show ( head $ map (\(x,y) -> x) keys) ++ ";" ++ show ( head $ map (\(x,y) -> y) keys)
     writeFile privName $ show ( last $ map (\(x,y) -> x) keys) ++ ";" ++ show ( last $ map (\(x,y) -> y) keys)
-    --writeFile pubName  $ show $ head keys
-    --writeFile privName $ show $ last keys
     return ()
-
