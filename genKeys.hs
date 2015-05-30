122410d25208f5226486f07c994b1c322813e1b6
h3nnn4n <uber.renan@gmail.com>
2015-05-30 20:08:06 +0000

    Writing keys using csv

--- genKeys.hs
+++ genKeys.hs
@@ -7,16 +7,17 @@ import System.IO.Error
 wrapper k = return $ genKeys k
 
 main = do
+    -- Gets the arguments, first one is the name for keyfile and the second is the key size
     args <- getArgs
     keys <- wrapper (read (args !! 1) :: Integer)
-    --print $ head keys
-    --print $ keys !! 1
     let name = args !! 0
         pubName = name ++ "_pub.key"
         privName = name ++ "_priv.key"
     print pubName
     print privName
-    writeFile pubName $ show $ head keys
-    writeFile privName $ show $ head keys
+    writeFile pubName  $ show ( head $ map (\(x,y) -> x) keys) ++ ";" ++ show ( head $ map (\(x,y) -> y) keys)
+    writeFile privName $ show ( last $ map (\(x,y) -> x) keys) ++ ";" ++ show ( last $ map (\(x,y) -> y) keys)
+    --writeFile pubName  $ show $ head keys
+    --writeFile privName $ show $ last keys
     return ()
 
