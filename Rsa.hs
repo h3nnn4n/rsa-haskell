fc5516c99c6caecd6bd2795610a1a09cdb0199e7
h3nnn4n <uber.renan@gmail.com>
2015-05-31 22:03:39 +0000

    Key size now in bits

--- Rsa.hs
+++ Rsa.hs
@@ -3,23 +3,19 @@ module Rsa where
 import ExtendedEuclides
 import Prime
 
---genKeys k = [(p,q),(e ,p*q),(d,p*q)]
-genKeys k = [(e ,p*q),(d,p*q)]
+--genKeys k = [(p, q), (e,p * q), (d, p * q)]
+genKeys k = [(e, p * q) , (d, p * q)]
     where
         e
-         | k < 5     = findPrime (k-1) 32
-         | k <= 1    = error "too small"
-         | otherwise = findPrime 5 32
+         | k < 16    = findPrime (k-1) 32
+         | k <= 4    = error "too small"
+         | otherwise = findPrime 16 32
         p = findPrime k 32
         q = findPrime k 32
         d = extendedEuclid (getPhi p q) e
-        --d = dropWhile (\x -> (extendedEuclid (getPhi p q) e) == 0) [1..]
             where
-                getPhi p q = (p-1)*(q-1)
---getPhi p q = (p-1)*(q-1)
+                getPhi p q = (p - 1) * (q - 1)
 
 cypher m (e, n) = powm m e n 1
 
 decypher m (d, n) = powm m d n 1
-
-
